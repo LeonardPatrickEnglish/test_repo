957e4822a45ec2291b9059a4e40f8959e550d83f
LeonardPatrickEnglish <leonardpatrickenglish@gmail.com>
2019-08-10 17:45:00 +0100

    first commit

---
+++ test_copy.py
@@ -0,0 +1,17 @@
+# Credit to Corey Schafer for this tutorial: https://coreyms.com/
+
+def add(x,y):
+   pass
+
+def subtract(x,y):
+    pass    
+
+def mulitply(x,y):
+    pass
+
+def divide(x,y):
+    pass
+
+def mod(x,y):
+    pass
+
